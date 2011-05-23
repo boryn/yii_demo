18da38ab79249664bf8ab56a092dcb1b545a9457
Michal Borychowski <mb-git@dynasoft.pl>
2011-05-23 22:57:37 +0200

    config: mysql enabled

--- protected/config/main.php
+++ protected/config/main.php
@@ -47,19 +47,20 @@ return array(
 			),
 		),
 		*/
+		/*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
+		*/
 		// uncomment the following to use a MySQL database
-		/*
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=yii_demo',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
+			'username' => 'yii',
+			'password' => 'vdHjTxJhrUH9',
 			'charset' => 'utf8',
 		),
-		*/
+		
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
             'errorAction'=>'site/error',
