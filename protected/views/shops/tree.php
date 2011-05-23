3de12e833e175870a1fe1fb73af9c90940d59269
Michal Borychowski <mb-git@dynasoft.pl>
2011-05-24 00:30:23 +0200

    shops_tree: view with expandable voivodships filled with data by AJAX

--- protected/views/shops/tree.php
+++ protected/views/shops/tree.php
@@ -5,7 +5,10 @@ $this->breadcrumbs=array(
 );?>
 <h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
 
-<p>
-	You may change the content of this page by modifying
-	the file <tt><?php echo __FILE__; ?></tt>.
-</p>
+
+<?php
+$this->widget(
+    'CTreeView',
+    array('url' => array('ajaxFillTree'))
+);
+?>
