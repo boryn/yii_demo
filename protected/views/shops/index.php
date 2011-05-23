9465aadbbac142a728fd60cc7f1a2683fe43e7d5
Michal Borychowski <mb-git@dynasoft.pl>
2011-05-24 00:09:10 +0200

    shops_list: view with shops list created

--- protected/views/shops/index.php
+++ protected/views/shops/index.php
@@ -4,7 +4,13 @@ $this->breadcrumbs=array(
 );?>
 <h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
 
-<p>
-	You may change the content of this page by modifying
-	the file <tt><?php echo __FILE__; ?></tt>.
-</p>
+<?php 
+$this->widget('zii.widgets.grid.CGridView', array(
+ 'dataProvider'=>$model->search(),
+ 'filter'=>$model,
+ 'columns'=>array(
+	'voivodship', 'city', 'street', 'place'
+ ),
+)); 
+
+?>
